56d78a8fdffb4d392dbce6eb7a929a8da7f8c8be
Derek Smyth <smyth.derek@gmail.com>
2023-03-10 08:54:01 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+// // Add your code here
+
+function submitData(name, email) {
+    const configObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            'name': name,
+            'email': email
+        })
+    }
+    return fetch('http://localhost:3000/users', configObject)
+    .then(response => response.json())
+    .then(data => addNewContact(data.id))
+    .catch(error => document.body.innerHTML = error.message)
+}
+
+function addNewContact(newID) {
+    document.getElementsByTagName("body")[0].innerHTML = newID;
+}
